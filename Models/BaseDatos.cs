4c282c0b057998ec083fce43ebb309164edc2ab5
Romeo <romeojeir@gmail.com>
2022-05-19 17:57:53 -0300

    Nineth commit

---
+++ Models/BaseDatos.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace GestionARG.Models
+{
+    public class BD
+    {
+        private static string _connectionString = @"Server=DESKTOP-4CCAH31;DataBase=GestionARG;Trusted_Conecction=True;";
+    }
+}
