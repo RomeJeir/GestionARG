80fe9d0a0d7a655c38c083128f7da3f0d8760931
ROMEO JEIRFIMCZUK ALMEIDA <46583925@ort>
2022-05-20 11:06:55 -0300

    Tenth commit

--- Models/BaseDatos.cs
+++ Models/BaseDatos.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
+using Dapper;
 
 namespace GestionARG.Models
 {
-    public class BD
+    public class BaseDatos
     {
         private static string _connectionString = @"Server=DESKTOP-4CCAH31;DataBase=GestionARG;Trusted_Conecction=True;";
+
+     /*    public static Persona ConsultaGestionARG(int IdEmpleado)
+        {
+            Persona IdEmpleado = null;
+            using (SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string sql = "SELECT Nombre, Apellido, IdEstablecimiento, Voto, NumeroTramite FROM Personas WHERE DNI"; 
+            }
+            return IdEmpleado;
+        }*/
     }
 }
