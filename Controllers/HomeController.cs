1ccc5cefb65a297c08217de56651938664a3b3b4
ROMEO JEIRFIMCZUK ALMEIDA <46583925@ort>
2022-05-27 09:25:46 -0300

    Seventeenth commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,7 +9,7 @@ using System.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 using GestionARG.Models;
 
-namespace GestionARG.Controllers;
+namespace GestionARG.Controllers{
 
 public class HomeController : Controller
 {
@@ -19,10 +19,10 @@ public class HomeController : Controller
         return View();
     }
 
-     public IActionResult VerEmpleados(int IdEmpleado)
+    public IActionResult VerEmpleados(int IdEmpleado)
         {
 
-            string cadena = @"Server=DESKTOP-BSJ52N9\MSQLSERVER;DataBase=GestionARG;Trusted_Connection=True;";
+            string cadena = @"Server=A-CBO-04;DataBase=GestionARG;Trusted_Connection=True;";
             SqlConnection con = new SqlConnection(cadena);
             SqlDataAdapter da = new SqlDataAdapter("SELECT NOMBRE, DNI, AREA, DESCRIPCION FROM Empleados",con);
             DataTable dt = new DataTable();
@@ -31,5 +31,11 @@ public class HomeController : Controller
 
             return View(dt);
 
-        } 
+        }
+
+    public IActionResult ControlEmpleados()
+    {
+        return View();
+    }
+}
 }
