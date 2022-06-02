54d17cf7e5c4737614ca79797fb9fd6e5778e8e1
Romeo <romeojeir@gmail.com>
2022-06-02 17:49:43 -0300

    Eighteenth commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,7 +22,7 @@ public class HomeController : Controller
     public IActionResult VerEmpleados(int IdEmpleado)
         {
 
-            string cadena = @"Server=A-CBO-04;DataBase=GestionARG;Trusted_Connection=True;";
+            string cadena = @"Server=DESKTOP-BSJ52N9\MSQLSERVER;DataBase=GestionARG;Trusted_Connection=True;";
             SqlConnection con = new SqlConnection(cadena);
             SqlDataAdapter da = new SqlDataAdapter("SELECT NOMBRE, DNI, AREA, DESCRIPCION FROM Empleados",con);
             DataTable dt = new DataTable();
@@ -53,9 +53,43 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult SubidaEmpleados()
-    {
+
+    public IActionResult SubidaEmpleados(){
         return View();
+    }
+    public IActionResult SubidaEmpleadosPOST(string nombre, int dni, string area, string descripcion, string direccion)
+    {
+
+        try
+
+        {
+
+        string cadena = @"Server=DESKTOP-BSJ52N9\MSQLSERVER;DataBase=GestionARG;Trusted_Connection=True;";
+        SqlConnection con = new SqlConnection(cadena);
+        string sentencia = new String.Format("INSERT INTO Empleados (NOMBRE, DNI, AREA, DESCRIPCION, DIRECCION) VALUES ('{0}', '{1}', '{2}' '{3}', '{4}')", nombre, dni, area, descripcion, direccion);
+        SqlDataAdapter com = SqlCommand(sentencia, con);
+
+        con.Open();
+        int cantidadFilasAfectada = com.ExecuteNonQuery();
+        con.Close();
+
+        if(cantidadFilasAfectadas != 1){
+            throw new ApplicationException("No se agrego el empleado");
+        }
+
+        TempData["MSG"] = "Se subio el empelado con exito" + string.Format("{0} {1} {2} {3} {4}", nombre, dni, area, descripcion, direccion);
+
+        return View("SubidaEmpleados");
+
+        }
+        catch (System.Exception ex)
+        {
+            
+            TempData["MSG"] = "No se agrego... " + string.Format("{0} {1} {2} {3} {4}", nombre, dni, area, descripcion, direccion, ex.Message);
+            return View("SubidaEmpleados");
+        }
+
+
     }
 }
 }
